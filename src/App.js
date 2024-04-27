3f2d453ad5ae44a5b40a5529c02a6928b311334b
Fred <fredtodd101@gmail.com>
2024-04-27 17:57:55 +0100

    setting up

--- src/App.js
+++ src/App.js
@@ -3,15 +3,12 @@
 import React from 'react';
 import './App.css'; // Import the CSS file
 import Header from './Header'; // Import the Header component
-import { ThemeProvider } from './Theme'; // Import the ThemeProvider
 
 function App() {
   return (
-    <ThemeProvider>
-      <div className="App">
-        <Header /> {/* Render the Header component */}
-      </div>
-    </ThemeProvider>
+    <div className="App">
+      <Header /> {/* Render the Header component */}
+    </div>
   );
 }
 
